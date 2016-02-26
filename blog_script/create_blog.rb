a6d4586c8807a88ad04266fb9ea1be880c13d282
jonathan <jmayran@ggc.edu>
2016-02-26 04:42:15 +0000

    added script

--- blog_script/create_blog.rb
+++ blog_script/create_blog.rb
@@ -1,3 +1,37 @@
-def new_blog()
-  puts "Please enter the place name: "
-end
+blog = File.new('newblog.html', 'a')
+blog.write("<head>
+    <title>Jonathan's Homepage</title>
+    
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+
+</head>
+
+
+<body>
+    <div class="navigation">
+        <ul>
+            <li><a href="index.html"><strong>Home</strong></a></li>
+            <li><a href="projects.html"><strong>Projects</strong></a></li>
+            <li><a href="resume.html"><strong>Resume</strong></a></li>
+            <li><a href="hobbies.html"><strong>Hobbies</strong></a></li>
+        </ul>
+    </div>"
+
+loop = true
+    while do
+        puts "Please enter the name of a place: "
+        placename = gets.chomp
+        puts "Please enter the picture's URL: "
+        pictureurl = gets.chomp
+        puts "Plase enter a description of the place: "
+        description = gets.chomp
+    
+    Blog.write("newstuff += "<h1>" + placename + "</h1>" + "\n"
+                newstuff += "<img src= " + pictureurl + ">" + "\n"
+                newstuff += "<p>" + description + "</p>" + "\n"")
+    
+        puts "Do you want to add another blog post? (y or n)"
+        if anotherpost == "no"
+            loop = false
+        end
+    end
